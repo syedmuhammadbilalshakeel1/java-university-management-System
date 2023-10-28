ef1218fb5691f283c52982a5b4465b652d159e8f
syed muhammad bilal <102545337+syedmuhammadbilalshakeel1@users.noreply.github.com>
2023-10-28 19:32:26 +0500

    the login and some of the database has to be done

---
+++ src/school/conter.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package school;
+
+/**
+ *
+ * @author Haris Khan
+ */
+class conter {
+
+    static void SetText(String sum) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
